e0c59012304669f51d6a3b35e001bcb8b5de33b4
Harith Albadry <harith.albadry@gmail.com>
2021-05-01 17:51:34 +0100

    card improvement, added icons from font awesome

--- index.js
+++ index.js
@@ -139,10 +139,10 @@ const createIntern = (data) => {
 
  const managerCardRender= () =>{
 	for(i=0; i<managArr.length; i++)
-	{ manfile = `<div class="card col-3" >
+	{ manfile = `<div class="card col-4" >
 	<div class="card-body manager">
-	  <h3 class="card-title">Manager</h3>
-	  <h6 class="card-subtitle mb-2 text-muted">icon</h6>
+	  <h3 class="card-title">Manager <i class="fas fa-user"></i></h3>
+	  
 	  <div class="cardtweak">
 		<p class="card-text">Name:${managArr[i].name}</p>
 		<p class="card-text">ID: ${managArr[i].id}</p>
@@ -156,10 +156,10 @@ const createIntern = (data) => {
 const engineerCardRender = () => {
 	let finalEngineer=""
 	for(i=0; i<enginArr.length; i++)
-	{ engineerFile = `<div class="card col-3" >
-	<div class="card-body manager">
-	  <h3 class="card-title">Engineer</h3>
-	  <h6 class="card-subtitle mb-2 text-muted">icon</h6>
+	{ engineerFile = `<div class="card col-4" >
+	<div class="card-body engineer">
+	  <h3 class="card-title">Engineer <i class="fab fa-dev"></i></h3>
+	  
 	  <div class="cardtweak">
 		<p class="card-text">Name:${enginArr[i].name}</p>
 		<p class="card-text">ID: ${enginArr[i].id}</p>
@@ -178,10 +178,10 @@ const engineerCardRender = () => {
 const internCardRender = () => {
 	let finalIntern=""
 	for(i=0; i<internArr.length; i++)
-	{ internFile = `<div class="card col-3" >
-	<div class="card-body manager">
-	  <h3 class="card-title">Intern</h3>
-	  <h6 class="card-subtitle mb-2 text-muted">icon</h6>
+	{ internFile = `<div class="card col-4" >
+	<div class="card-body intern">
+	  <h3 class="card-title">Intern <i class="fas fa-edit"></i></h3>
+	  
 	  <div class="cardtweak">
 		<p class="card-text">Name:${internArr[i].name}</p>
 		<p class="card-text">ID: ${internArr[i].id}</p>
@@ -206,19 +206,19 @@ const htmlRender = () => {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 		<link href="testpage.css" rel="stylesheet">
+		<script src="https://kit.fontawesome.com/a8758c57fb.js" crossorigin="anonymous"></script>
 		<title>Document</title>
 	</head>
 	<body>
 		<div class="jumbotron jumbotron-fluid">
 			<div class="container">
 			  <h1 class="display-4">My Team</h1>
-			  <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
 			</div>
 		  </div>
 		  <section class="container ">\n
 		  ${managerCardRender()}\n
 		  </section>
-		  <section class="container container-fluid">
+		  <section class="container">
 		  <div class="row">\n
 		  ${engineerCardRender()}\n
 		  </div>
